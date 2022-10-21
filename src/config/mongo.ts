0e4fded43f18af27efef5fbc58a2be649b808c96
Marek Połom <marekpolom22@gmail.com>
2022-10-22 00:15:39 +0200

    add mongo and passport

---
+++ src/config/mongo.ts
@@ -0,0 +1,31 @@
+import serverConfig from "./serverConfig";
+import mongoose, { ConnectOptions } from "mongoose";
+
+const username = serverConfig.mongoConfig.username;
+const password = serverConfig.mongoConfig.password;
+const host = serverConfig.mongoConfig.host;
+const port = serverConfig.mongoConfig.port;
+const database = serverConfig.mongoConfig.database;
+
+const mongoHost = `mongodb://${host}:${port}`;
+mongoose.connect(mongoHost, {
+  user: username,
+  pass: password,
+  dbName: database
+});
+
+mongoose.connection
+  .on("connected", () => {
+    console.log("Connected to MongoDB");
+  })
+  .on("error", (error) => {
+    console.log("Error on MongoDB connection\n", error);
+  })
+  .on("disconnected", () => {
+    console.log("Disconnected from MongoDB");
+  });
+
+export const Schema = mongoose.Schema;
+export const model = mongoose.model;
+
+export default mongoose;
---
+++ src/config/session.ts
@@ -0,0 +1,10 @@
+import mongoStore from "./mongoStore";
+import session from 'express-session';
+import serverConfig from "./serverConfig";
+
+export default session({
+  secret: serverConfig.secret,
+  saveUninitialized: false,
+  resave: false,
+  store: mongoStore
+});
